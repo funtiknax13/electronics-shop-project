3a7f20809dfe5f2b253309bef1fb1bdbb8a41ea9
funtiknax13 <sergeyfedorov13@yandex.ru>
2023-04-21 21:31:15 +0300

    add class Init and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+item1 = Item("Веб-камера", 5000, 15)
+item2 = Item("Флешка", 500, 100)
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 75000
+    assert item2.calculate_total_price() == 50000
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 2500
+    item2.apply_discount()
+    assert item2.price == 250
+
